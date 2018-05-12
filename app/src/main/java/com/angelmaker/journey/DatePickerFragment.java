049a43082a2bed982ea0a664a1750261cfbc2cf8
Angel-Maker <AngelMakerContact@gmail.com>
2018-05-12 08:23:48 -0600

    Added date pickers to new activity selection

---
+++ app/src/main/java/com/angelmaker/journey/DatePickerFragment.java
@@ -0,0 +1,58 @@
+package com.angelmaker.journey;
+
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+
+
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+    private TextView dateTV;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        //Todo Decide whether to use Holo_Light or calendar view
+        // Create a new instance of DatePickerDialog and return it
+        DatePickerDialog dialog = new DatePickerDialog(
+                getActivity(),
+                android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                this,
+                year,month,day);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        return dialog;
+    }
+
+    public void setTV(TextView newDateTV){dateTV = newDateTV;}
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        if(dateTV != null){
+            Log.i("selectedDate", "Date selected: " + year+"/"+month+"/"+day);
+            dateTV.setText(year+"/"+month+"/"+day);
+        }
+    }
+}
--- app/src/main/java/com/angelmaker/journey/NewActivity.java
+++ app/src/main/java/com/angelmaker/journey/NewActivity.java
@@ -4,10 +4,10 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+
+import android.widget.TextView;
 
 import com.angelmaker.journeyDatabase.ActivityInstance;
 
@@ -21,8 +21,11 @@ public class NewActivity extends AppCompatActivity {
     EditText activityNameET;
 
     private ActivityViewModel activityViewModel;
+    private TextView startDateTV;
+    private TextView endDateTV;
 
     private SimpleDateFormat sdfDB = new SimpleDateFormat("yyyy/MM/dd", Locale.getDefault());
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,42 +35,58 @@ public class NewActivity extends AppCompatActivity {
         activityViewModel = ViewModelProviders.of(this).get(ActivityViewModel.class);
 
         activityNameET = findViewById(R.id.activityNameET);
+        startDateTV = findViewById(R.id.startDateTV);
+        endDateTV = findViewById(R.id.endDateTV);
 
 
-        final Button button = findViewById(R.id.SubmitBtn);
-        button.setOnClickListener(new View.OnClickListener()
-        {
+        linkDatePickerFragment(startDateTV);
+        linkDatePickerFragment(endDateTV);
+    }
+
+
+
+    public void linkDatePickerFragment(final TextView textView){
+        textView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view)
             {
-                if (TextUtils.isEmpty(activityNameET.getText())) {}
-                else {
-                    String activityName = activityNameET.getText().toString();
-                    String activityDescription = "No Description";
-                    String activityStartDate = "2018/05/10";
-                    String activityEndDate = "2018/05/19";
-
-                    int day = 0;
-                    List<ActivityInstance> newFullActivity = new ArrayList<ActivityInstance>();
-                    while (day < 10)
-                    {
-                        ActivityInstance newActivity = new ActivityInstance();
-                        newActivity.setActivityName(activityName);
-                        newActivity.setActivityDescription(activityDescription);
-                        newActivity.setStartDate(activityStartDate);
-                        newActivity.setEndDate(activityEndDate);
-                        newActivity.setCurrentDate("2018/05/1" + Integer.toString(day));
-
-                        newFullActivity.add(newActivity);
-
-                        day++;
-                    }
-
-                    activityViewModel.insertMany(newFullActivity);
-                }
-
-                finish();
+                DatePickerFragment datePicker = new DatePickerFragment();
+                datePicker.setTV(textView);
+                datePicker.show(getFragmentManager(), "DatePicker");
             }
         });
+    }
+
+
+
+    public void SubmitBtnClicked (View view)
+    {
+        if (!TextUtils.isEmpty(activityNameET.getText()))
+        {
+            String activityName = activityNameET.getText().toString();
+            String activityDescription = "No Description";
+            String activityStartDate = "2018/05/10";
+            String activityEndDate = "2018/05/19";
+
+            int day = 0;
+            List<ActivityInstance> newFullActivity = new ArrayList<ActivityInstance>();
+            while (day < 10)
+            {
+                ActivityInstance newActivity = new ActivityInstance();
+                newActivity.setActivityName(activityName);
+                newActivity.setActivityDescription(activityDescription);
+                newActivity.setStartDate(activityStartDate);
+                newActivity.setEndDate(activityEndDate);
+                newActivity.setCurrentDate("2018/05/1" + Integer.toString(day));
+
+                newFullActivity.add(newActivity);
+
+                day++;
+            }
+
+            activityViewModel.insertMany(newFullActivity);
+        }
+
+        finish();
     }
 }
